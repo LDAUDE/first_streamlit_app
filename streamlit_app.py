94fb5a922056d8efed4baccf72a98244ed697c8d
LDAUDE <95637930+LDAUDE@users.noreply.github.com>
2023-02-22 09:52:51 +0100

    Update streamlit_app.py
    Added a new specific Personal Smoothie Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text("Omega 3 & Blueberry Oatmeal 🥣")
 streamlit.text("Kale, Spinach and Rocket Smoothie 🥗")
 streamlit.text("Hard-Boiled  Free-Range Egg 🍳")
 streamlit.text("Avocado Toast 🥑🍞")
+
+streamlit.header("🍌🍍 Build your Own Fruit Smoothie 🥝")
